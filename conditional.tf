93ebb24f3a980f996ffd82f04a6d68fbda38a33f
atul gite <atulsss.gite@gmail.com>
2021-07-24 22:42:47 +0530

    removed sensitive data

--- conditional.tf
+++ conditional.tf
@@ -29,15 +29,15 @@ resource "aws_instance" "prod1" {
 /*provider "aws" {
 
   region = "ap-south-1"
-  access_key = "AKIAXU3DJZYJEYFDRFCW"
-  secret_key = "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"
+  access_key = "XXXX"
+  secret_key = "XXXX"
 }
 
 variable istest {}
 
 resource "aws_instance" "dev1" {
 
-    ami = "ami-0ad704c126371a549"
+    ami = "ami-XXXX"
     instance_type ="t2.micro"
 
     count = var.istest == true ? 3 : 0 #if var.istest value is true, then 3 instances will be created
@@ -45,11 +45,11 @@ resource "aws_instance" "dev1" {
 
 resource "aws_instance" "prod1" {
 
-   ami = "ami-0ad704c126371a549"
+   ami = "ami-XXXX"
    instance_type = "t2.nano"
 
    count = var.istest == false ? 1 : 0 #if var.istest value is false, then 1 instance will be created
 
 }
 */
->>>>>>> 8c483f02ab868b89a4c06bca481bc10251c496d9
+>>>>>>> 
